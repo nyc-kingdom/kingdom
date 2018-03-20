4530bd45e2b3b201a9ecddf2099cf3c429fe0144
Dongwoo Kang <schach0415@yahoo.com>
2018-03-20 09:35:42 -0400

    clean more

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -20,19 +20,25 @@ export class Dash extends React.Component {
             <div id="Dash">
                 <button onClick={()=>{getUserCheckIns(this)}}>FETCH MY CHECK-INS</button>
                 <div>{'HEY BOY ' + JSON.stringify(this.props.user)}</div>
-                <input id='userInput' value={this.state.userInput} onChange={evt=>{this.setState({userInput: evt.target.value})}}></input>
+                <input
+                    id='userInput'
+                    value={this.state.userInput}
+                    onChange={evt=>{this.setState({userInput: evt.target.value})}}
+                />
                 <button onClick={() => { locationQuery(this) }}>Hello</button>
-
                 {
                     this.state.queriedMarkers.length > 0 && this.state.queriedMarkers.map(eachMarker => (
-                        <div key={eachMarker.venue.id} >
-                            <Link to={`/singleEstablishment/${eachMarker.venue.id}`}>{eachMarker.venue.name}</Link>
-                            <button onClick={()=>{checkIn(this, eachMarker)}}>Check In!</button> 
-                        </div>
-                    ))
+                            <div key={eachMarker.venue.id} >
+                                <Link to={`/singleEstablishment/${eachMarker.venue.id}`}>
+                                    {eachMarker.venue.name}
+                                </Link>
+                                <button onClick={()=>{checkIn(this, eachMarker)}}>Check In!</button> 
+                            </div>
+                        )
+                    )
                 }
                 <a href='http://localhost:8080/auth/foursquare'><button>Login</button></a>
-             <a href='http://localhost:8080/auth/foursquare'><button>Signup</button></a>
+                <a href='http://localhost:8080/auth/foursquare'><button>Signup</button></a>
             </div>
         )
     }
