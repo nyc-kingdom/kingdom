50d38ac0574d0bfef3b8e705674b03a14c7b8d14
Bruce Leong <brucewleong@gmail.com>
2018-03-20 16:59:19 -0400

    updated

---
+++ src/components/Home.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createMarker } from '../store';
+
+import locationQuery, { checkIn, getUserCheckIns } from '../functions/locationQuery'
+
+export class Home extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      queriedMarkers: [],
+      userInput: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => { getUserCheckIns(this) }}>FETCH MY CHECK-INS</button>
+        <h1>Kingdom</h1>
+        <button onClick={() => { locationQuery(this) }}>Hello</button>
+        {
+          this.state.queriedMarkers.length > 0 && this.state.queriedMarkers.map(eachMarker => (
+            <div key={eachMarker.venue.id} >
+              <Link to={`/singleEstablishment/${eachMarker.venue.id}`}>
+                {eachMarker.venue.name}
+              </Link>
+              <button onClick={() => { checkIn(this, eachMarker) }}>Check In!</button>
+            </div>
+          )
+          )
+        }
+        <a href='http://localhost:8080/auth/foursquare'><button>Login</button></a>
+        <a href='http://localhost:8080/auth/foursquare'><button>Signup</button></a>
+      </div>
+    )
+  }
+}
+
+const mapProps = state => ({
+  markers: state.markers,
+  user: state.user
+})
+
+const mapDispatch = dispatch => ({
+  createMarker: marker => dispatch(createMarker(marker))
+})
+
+export default connect(mapProps, mapDispatch)(Home)
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Map } from './Map';
 export { default as Dash } from './Dash';
 export { default as Markers } from './Markers'
+export { default as Home } from './Home'
 export { default as HomeDash } from './HomeDash'
 export { default as Routes } from './Routes'
 export { default as YourProfile } from './YourProfile'
