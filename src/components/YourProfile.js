62aeac10b36f6748c3f5958d1a1ff3fc01fd7dbf
Dongwoo Kang <schach0415@yahoo.com>
2018-03-22 11:48:03 -0400

    before merge

--- src/components/YourProfile.js
+++ src/components/YourProfile.js
@@ -3,67 +3,146 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const hardCoding = {
-    kingdomImgUrl: "https://i.pinimg.com/originals/0d/26/fd/0d26fd531a191bdf6659fd0b9ef4c73c.png",
-    kingdomName: "No Kingdom",
-    userName: "Dongwoo Kang",
-    userImgUrl: "https://elwiki.net/wiki/images/thumb/e/ef/Lord_Knight.png/500px-Lord_Knight.png",
-    keeperChairUrl: "https://banner.kisspng.com/20180208/wqw/kisspng-throne-chair-icon-throne-5a7c0d10b42472.6808909515180792487379.jpg",
-    kingName: "No King",
-    level: "Knight",
-    points: 200,
+    flagBackgroundImgUrl: "https://i.pinimg.com/originals/0d/26/fd/0d26fd531a191bdf6659fd0b9ef4c73c.png",
+    keeperChairUrl: "https://cdn4.iconfinder.com/data/icons/knight/512/as416g_7-512.png",
     xButton: "http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons-256/ultra-glossy-silver-buttons-icons-alphanumeric/075091-ultra-glossy-silver-button-icon-alphanumeric-x-styled.png",
+    flagKingFaceImgurl: "https://cdn.pixabay.com/photo/2016/08/06/14/11/lion-1574448_960_720.png",
+    user: {
+        id: 1,
+        name: "Dongwoo Kang",
+        imgUrl: "https://i.imgur.com/I8rXtBd.png",
+        level: "Knight",
+        points: 200,
+        kingdom: {
+            name: "Lynbrook",
+            imgUrl: "http://leightronix.com/customers/images/lynbrook/Lynbrook_Logo.png",
+            king: {
+                id: 1,
+                name: "Dongwoo Kang"
+            }
+        }
+    },
+    establishment: {
+        id: 1,
+        name: "Liquor & Wine Warehouse",
+        address: "343 Rockaway Tpke Lawrence, NY 11559",
+        imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/8172-200.png",
+        level: "Castle",
+        points: 20,
+        totalVisit: 210,
+        totalAttect: 100,
+        keeper: 
+        // null,
+        {
+            id: 1,
+            name: "Dongwoo Kang",
+            imgUrl: "https://elwiki.net/wiki/images/thumb/e/ef/Lord_Knight.png/500px-Lord_Knight.png",    
+        },
+        kingdom: {
+            id: 1,
+            name: "Lynbrook",
+            imgUrl: "http://leightronix.com/customers/images/lynbrook/Lynbrook_Logo.png",
+            king: {
+                name: "Dongwoo Kang"
+            }
+        }
+    },
+    kingdom: {
+        id: 1,
+        name: "Lynbrook",
+        imgUrl: "http://leightronix.com/customers/images/lynbrook/Lynbrook_Logo.png",
+        level: "Great Kingdom",
+        points: 2000,
+        king: {
+            id: 1,
+            name: "Dongwoo Kang"
+        }
 
+    }
 }
 
 export class Profile extends React.Component {
     constructor(props) {
         super(props)
-        this.renderUser = this.renderUser.bind(this)
+        // this.renderUser = this.renderUser.bind(this)
 
     }
 
-
-
     render(){
-        const main = this.props.main // please pass 'user', 'establishment', or 'kingdom'
-        const levelUpPoints = 1000
+        // please pass main={'user', 'establishment', or 'kingdom'}
+        const example = 'establishment'
+        const main = !this.props.main ? hardCoding[example] : this.props.main
+        const levelUpPoints = 3000
         return (
             <div>
-                <div>
-                    <div>
-                        <img src={!main.kingdom ? hardCoding.kingdomImgUrl : main.kingdom.imgUrl}/>
-                        <h4>{!main.kingdom ? hardCoding.kingdomName : main.kingdom.name}</h4>
-                    </div>
-                    <div>
-                        <h1>{!main.name ? hardCoding.userName : main.name}</h1>
+                <div style={{height: '5vh'}}/>
+                <div style={{display: 'flex'}}>
+                    <div style={{flex: 1}}>
                         <div>
-                            <img src={!main.imgUrl ? hardCoding.userImgUrl : main.imgUrl}/>
-                            {
-                                !main.address ?
-                                null : !main.keeper ?
-                                <img src={hardCoding.keeperChairUrl}/> :
-                                (
-                                <div>
-                                    <img scr={main.keeper.imgUrl}/>
-                                    <h5>{main.keeper.name}</h5>
-                                </div>
-                                )
-                            }
+                            <img
+                                src={hardCoding.flagBackgroundImgUrl}
+                                style={{width:'25vw', left: 0}}
+                            />
+                            <Link to={`/yourProfile?kingdom=${main.kingdom.id}`}>
+                                <img
+                                    src={!main.kingdom ? main.imgUrl : main.kingdom.imgUrl}
+                                    style={{width: '13vw', position: 'absolute', left: '4.5vw', top: '10vh'}}
+                                />
+                            </Link>
                         </div>
+                        {/* <span>{main.kingdom.name}</span> */}
                     </div>
-                    <div>
-                        <img src={!main.kingdom ? hardCoding.kingdomImgUrl : main.kingdom.imgUrl}/>
-                        <h4>{!main.kingdom ? hardCoding.kingName : main.king.name}</h4>
+                    <div style={{flex: 2, textAlign: 'center'}}>
+                        <h3>{main.name}</h3>
+                    </div>
+                    <div style={{flex: 1}}>
+                        <div>
+                            <img
+                                src={hardCoding.flagBackgroundImgUrl}
+                                style={{width:'25vw', right: 0}}
+                            />
+                            <img
+                                src={hardCoding.flagKingFaceImgurl}
+                                style={{width: '13vw', position: 'absolute', right: '7.5vw', top : '8vh'}}
+                            />
+                        </div>
+                        {/* <span>{main.kingdom.king.name}</span> */}
                     </div>
                 </div>
                 <div>
-                    <h3>Level : {!main.level ? hardCoding.level : main.level}</h3>
-                    <div>{!main.points ? hardCoding.points : main.points} / {levelUpPoints}</div>
-                    <h3>{!main.points ? hardCoding.points : main.points} / {levelUpPoints}</h3>
+                    <div style={{textAlign: 'center'}}>
+                        <img src={main.imgUrl} style={{width: '75vw', height: '80vw'}}/>
+                        {
+                            !main.address ?
+                            null : !main.keeper ?
+                            (
+                            <div style={{position: 'absolute', top: '44vh'}}>
+                                <img src={hardCoding.keeperChairUrl} style={{width: '45vw'}}/>
+                            </div>
+                            ) :
+                            (
+                            <div style={{position: 'absolute', top: '47vh'}}>
+                                <img src={main.keeper.imgUrl} style={{width: '45vw'}}/>
+                                {/* <h5>{main.keeper.name}</h5> */}
+                            </div>
+                            )
+                        }
+                    </div>
                 </div>
-                <div></div>
+                <div style={{textAlign: 'center'}}>
+                    <span>Level : {main.level}</span>
+                    <div>{main.points} / {levelUpPoints}</div>
+                    <span>{main.points} / {levelUpPoints}</span>
+                </div>
                 <div>
-                    <Link to='/main'><img scr={hardCoding.xButton}/></Link>
+                    {
+                        // main.map()
+                    }
+                </div>
+                <div style={{textAlign: 'center'}}>
+                    <Link to='/main'>
+                        <img src={hardCoding.xButton} style={{width: '12vw', height: '12vh'}}/>
+                    </Link>
                 </div>
             </div>
         )
@@ -91,7 +170,10 @@ export class Profile extends React.Component {
     // }
 }
 
-const mapProps = ({ user, checkins }) => ({ user, checkins })
+const mapProps = ({ user, checkins }, ownProps) => {
+    console.log('what is this? ',ownProps.location.search)
+    // const main = 
+}
 
 const mapDispatch = null
 
