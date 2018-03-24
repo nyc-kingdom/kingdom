424dcb7f020c428d646f5f09145dacf816b0b665
Connor Kirkwood <cdkirkwood@gmail.com>
2018-03-24 12:17:48 -0400

    pulling from master

--- src/components/Spotlight.js
+++ src/components/Spotlight.js
@@ -7,16 +7,16 @@ const Spotlight = props => {
 
     console.log('THIS IS YOUR CURRENT ', props.markers )
 
-    const place = props.markers.find(eachMarker=>eachMarker.venue.id===props.match.params.id)
+    const place = props.markers ? props.markers.find(eachMarker=>eachMarker.venue.id===props.match.params.id) : null
 
-    return (
+    return place ?
     <div id='Spotlight'>
     <h2>{place.venue.name + ' ' + '$'.repeat(place.venue.price.tier)}</h2>
     <button onClick={props.addCheckIn(props.user, place.venue)}>Check in here </button>
     <h3>{place.venue.stats.checkinsCount + ' people have checked in here.'}</h3>
     {place.tips && place.tips.map((eachTip,index)=>(<h4 key={index}>{eachTip.text}</h4>))}
     </div>
-)
+    :<h3>loading...</h3>
 }
 
 
