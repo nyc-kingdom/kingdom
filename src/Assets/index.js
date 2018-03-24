517bb9f40f3efebd1dc7f3e6b8ca6c90694dd4a0
Bruce Leong <brucewleong@gmail.com>
2018-03-24 14:22:31 -0400

    updated

---
+++ src/Assets/index.js
@@ -0,0 +1,16 @@
+import arrowLeft from './arrowLeft.png';
+import arrowRight from './arrowRight.png';
+import castle from './castle.png';
+import castleTower from './castleTower.png';
+import gem from './gem.png';
+import king from './king.gif';
+import lord from './Lord.gif';
+import shepard from './shepard1.gif';
+import bridgeShield from './shield2.png';
+import wolfShield from './shield1.png';
+import sword from './sword.png';
+import anvil from './anvil.png';
+import scroll from './scroll.png';
+import wand from './wand.png';
+
+export {arrowLeft, arrowRight, castle, castleTower, gem, king, lord, shepard, bridgeShield, wolfShield, sword, anvil, scroll, wand};
