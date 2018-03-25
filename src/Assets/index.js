99a03e6d808c69b5034328d30eb512a62989a486
Bruce Leong <brucewleong@gmail.com>
2018-03-24 23:35:18 -0400

    updated

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -1,17 +1,27 @@
 import arrowLeft from './arrowLeft.png';
 import arrowRight from './arrowRight.png';
+
 import castle from './castle.png';
 import castleTower from './castleTower.png';
+
+//resources
 import gem from './gem.png';
-import king from './king.gif';
-import lord from './Lord.gif';
-import shepard from './shepard.gif';
-import bridgeShield from './shield2.png';
-import wolfShield from './shield1.png';
 import sword from './sword.png';
 import anvil from './anvil.png';
 import scroll from './scroll.png';
 import wand from './wand.png';
-import knight from './knight.gif';
 
-export { arrowLeft, arrowRight, castle, castleTower, gem, king, lord, shepard, bridgeShield, wolfShield, sword, anvil, scroll, wand, knight };
+//characters
+import king from './king.gif';
+import lord from './Lord.gif';
+import shepard from './shepard.gif';
+import knight from './knight.gif';
+import blacksmith from './blacksmith.gif';
+import knightsword from './knightsword.gif';
+
+//kingdom art
+import bridgeShield from './shield2.png';
+import wolfShield from './shield1.png';
+import bushwickC from './bushwickC.png';
+
+export { arrowLeft, arrowRight, castle, castleTower, gem, king, lord, shepard, bridgeShield, wolfShield, sword, anvil, scroll, wand, knight, bushwickC, knightsword, blacksmith };
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createMarker } from '../store';
-import { Navigation } from './index'
-import king from '../Assets/king.gif'
-
+import Carousel from 'nuka-carousel';
+import { arrowRight, arrowLeft, king, shepard, lord, knight, knightsword, blacksmith } from '../Assets'
 
 export class Home extends React.Component {
 
@@ -13,17 +12,56 @@ export class Home extends React.Component {
   }
 
   render() {
+
+    const style = {
+      width: "100vw",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      textAlign: "center",
+    }
     return (
+
       <div className='home'>
         <br />
-        <h1>Kingdom</h1>
-        <br />
-        <img src={king} />
-        <br />
+        <h1 style={{ fontFamily: 'Apple Chancery, cursive', fontSize: '250%', textAlign: 'center'}}>Kingdom</h1>
+        <div>
+        <Carousel
+          renderCenterLeftControls={({ previousSlide }) => (
+            <img src={arrowLeft} onClick={previousSlide} />
+          )}
+          renderCenterRightControls={({ nextSlide }) => (
+            <img src={arrowRight} onClick={nextSlide} />
+          )}
+        >
+          <div className="carousel-summary" style={style}>
+            <img src={king} style={{width: "60vw", padding:"50px"}}/>
+          </div>
+          <div className="carousel-summary" style={style}>
+            <img src={shepard} style={{width: "60vw", padding:"50px"}}/>
+          </div>
+          <div className="carousel-summary" style={style}>
+            <img src={lord} style={{width: "60vw", padding:"50px"}}/>
+          </div>
+          <div className="carousel-summary" style={style}>
+            <img src={knight} style={{width: "60vw", padding:"50px"}}/>
+          </div>
+          <div className="carousel-summary" style={style}>
+            <img src={knightsword} style={{width: "60vw", padding:"50px"}}/>
+          </div>
+          <div className="carousel-summary" style={style}>
+            <img src={blacksmith} style={{width: "60vw", padding:"50px"}}/>
+          </div>
+        </Carousel>
+          <br />
+          <br />
+        </div>
         <a href='http://localhost:8080/auth/foursquare'><h2>Play Now</h2></a>
         <Link to='/about'>
           <h2>About</h2>
         </Link>
+
       </div>
     )
   }
@@ -39,3 +77,4 @@ const mapDispatch = dispatch => ({
 })
 
 export default connect(mapProps, mapDispatch)(Home)
+
