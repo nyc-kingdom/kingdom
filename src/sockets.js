57d64c2773f8a16b0d9724bff03c03129aaabfba
Dongwoo Kang <schach0415@yahoo.com>
2018-03-25 08:50:46 -0400

    shield update

--- src/sockets.js
+++ src/sockets.js
@@ -1,7 +1,9 @@
 const openSocket = require('socket.io-client')
 
 
-const socket = openSocket('http://localhost:8080', {transports: ['websocket']} )
+//const socket = openSocket('http://172.16.21.145:8080', {transports: ['websocket']} )
+
+const socket = openSocket('localhost:8080', {transports: ['websocket']} )
 
 
 
