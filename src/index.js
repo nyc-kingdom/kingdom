84650bd8765e32d0e79329e1c82053239d100127
Bruce Leong <brucewleong@gmail.com>
2018-03-28 09:31:13 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -10,31 +10,12 @@ import history from './history'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-const muiTheme = getMuiTheme(
-  {
-    "appBar": {
-        "padding": 40,
-        "color": "#9e9e9e",
-        "transparent": "true",
-        "textColor": "#673ab7",
-        "titleFontWeight": 600,
-        "height": 60
-    },
-    "bottomNavigation": {
-        "selectedColor": "#673ab7",
-        "unselectedColor": "#7c4dff",
-        "backgroundColor": "#9e9e9e"
-    }
-});
-
 ReactDOM.render(
-  <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
   <Provider store={store}>
     {/* <Router history={history}> */}
      <App />
     {/* </Router> */}
-  </Provider>
-  </MuiThemeProvider>,
+  </Provider>,
   document.getElementById('root'),
 );
 registerServiceWorker();
