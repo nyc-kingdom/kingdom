81f8081c8db5e2baf68d169fb668884bb50cac63
Bruce Leong <brucewleong@gmail.com>
2018-03-28 09:56:52 -0400

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -12,10 +12,35 @@ export class About extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Kingdom</h1>
-        <img src='https://kottke.org/plus/misc/images/8-bit-nyc.jpg' />
-        <h2>Kingdom is an awesome game you need in your life!</h2>
+      <div style={{ fontFamily: 'Apple Chancery, cursive', margin: '20px', fontSize: '110%'}}>
+        <h1>Kingdom Rules</h1>
+        <p>
+        Welcome to Kingdom: a game of neighborhood pride.
+
+
+        Pledge your Allegiance:
+        Sign up with your Foursquare account, and supply your address.
+        You will be assigned your kingdom based on the neighborhood you
+        live in.
+
+        Gain Experience:
+        Retrieve your Foursquare checkins to get a jumpstart on you Kingdom experience.
+        Begin checking in at local establishments to boost your personal experience,
+        and the strength of you kingdom.
+
+        Conquer an Establishment:
+        Each time a kingdom checks in to an establishment for the first time, a
+        new castle is forged. The Kingdom with the strongest castle (greatest number of
+        checkins), declares that establishment as part of their kingdom.
+
+        Become the keeper:
+        Checkin to an establishment more than anyone else and you become the keeper.
+        The keeper gains 1 experience point each time another player checks into
+        their establishment.
+
+        Become the King:
+        Own the most establishments within your kingdom, and become the king of your
+        realm.</p>
         <Link to={!this.props.user.id ? '/' : '/dashboard'} >
           <h3>Explore Now!</h3>
         </Link>
