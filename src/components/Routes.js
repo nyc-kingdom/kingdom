d11d82206c81d4c30828f22bd5d69b811ba687d5
Dongwoo Kang <schach0415@yahoo.com>
2018-04-16 19:41:33 -0400

    move router to front

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { Router, Route, Switch } from 'react-router-dom'
-import history from '../history'
 import { About, Home, HomeDash, Profile, SingleEstablishmentView, LeaderBoard, ItemList, ChangeKingdom, ChangeShield } from './index'
 
 const Pipeline = (props) => (
 
-    <Router history={history}>
-        <Switch>
-            {/* Routes placed here are available to all visitors */}
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            {
-                props.isLoggedIn &&
-                <Switch>
-                    {/* Routes placed here are only available after logging in */}
-                    <Route path="/dashboard" component={HomeDash} />
-                    <Route path="/profile/users/:user" component={Profile} />
-                    <Route path="/profile/kingdoms/:kingdom" component={Profile} />
-                    <Route path="/profile/establishments/:establishment" component={Profile} />
-                    <Route path="/items/users/:user/:item" component={ItemList} />
-                    <Route path="/items/kingdoms/:kingdom/:item" component={ItemList} />
-                    <Route path="/items/establishments/:establishment/:item" component={ItemList} />
-                    <Route path="/singleEstablishment/:id" component={SingleEstablishmentView} />
-                    <Route path="/leaderboard" component={LeaderBoard} />
-                    <Route path="/changeKingdom" component={ChangeKingdom} />
-                    <Route path="/changeShield" component={ChangeShield} />
-                </Switch>
-            }
-        </Switch>
-    </Router>
+    <Switch>
+        {/* Routes placed here are available to all visitors */}
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        {
+            props.isLoggedIn &&
+            <Switch>
+                {/* Routes placed here are only available after logging in */}
+                <Route path="/dashboard" component={HomeDash} />
+                <Route path="/profile/users/:user" component={Profile} />
+                <Route path="/profile/kingdoms/:kingdom" component={Profile} />
+                <Route path="/profile/establishments/:establishment" component={Profile} />
+                <Route path="/items/users/:user/:item" component={ItemList} />
+                <Route path="/items/kingdoms/:kingdom/:item" component={ItemList} />
+                <Route path="/items/establishments/:establishment/:item" component={ItemList} />
+                <Route path="/singleEstablishment/:id" component={SingleEstablishmentView} />
+                <Route path="/leaderboard" component={LeaderBoard} />
+                <Route path="/changeKingdom" component={ChangeKingdom} />
+                <Route path="/changeShield" component={ChangeShield} />
+            </Switch>
+        }
+    </Switch>
 )
 
 const mapProps = ({ user }) => {
