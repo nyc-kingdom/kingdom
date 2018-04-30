82edfafae7d603f61173b404d2fd0ee41a95ac33
Dongwoo Kang <schach0415@yahoo.com>
2018-04-30 14:53:13 -0400

    checkins from Foursquare Update realtime

--- src/store/establishments.js
+++ src/store/establishments.js
@@ -1,23 +1,44 @@
-import axios from 'axios';
+import axios from 'axios'
 import { serverUrl } from '../sockets'
+import { fetchCheckins } from './'
 
-
-const GET_ESTABLISHMENTS = 'GET_ESTABLISHMENTS';
+/**
+ * ACTION TYPES
+ */
+const GET_ESTABLISHMENTS = 'GET_ESTABLISHMENTS'
 const ADD_ESTABLISHMENT = 'ADD_ESTABLISHMENT'
 
-const getEstablishments = establishments => {
-  const action = {type: GET_ESTABLISHMENTS, establishments};
-  return action;
-}
-
+/**
+ * ACTION CREATORS
+ */
+const getEstablishments = establishments => ({type: GET_ESTABLISHMENTS, establishments})
 export const paintEstablishment = establishment => ({type: ADD_ESTABLISHMENT, establishment})
 
-export const fetchEstablishments = () => dispatch => {
-  return axios.get(`${serverUrl}/api/establishments`)
-  .then(establishments => dispatch(getEstablishments(establishments.data)))
-  .catch(console.error.bind(console))
+/**
+ * THUNK CREATORS
+ */
+export const fetchEstablishments = () => async dispatch => {
+  try {
+    const establishments = await axios.get(`${serverUrl}/api/establishments`)
+    dispatch(getEstablishments(establishments.data))  
+  } catch (err) {
+    console.error('Fetch Establishments unsuccessful.', err)
+  }
 }
 
+export const getUserCheckIns = user => async dispatch => {
+  try {
+    const payLoad = await axios.put(`${serverUrl}/api/establishments/foursquare`, {user})
+    dispatch(fetchCheckins())
+    dispatch(fetchEstablishments())
+  } catch (err) {
+    console.error('Get Checkins from Foursquare unsuccessful.', err)
+  }
+}
+
+/**
+ * REDUCER
+ */
 export default (state = [], action) => {
   switch (action.type) {
     case GET_ESTABLISHMENTS:
