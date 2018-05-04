09ff3e4d510a3d682ed48c86131dc222d67b0ace
Philip Gingerich <philipygingerich@gmail.com>
2018-05-04 15:57:03 -0400

    This commit re-nests the provider and the router under the app component. The app component should be the highest level component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import { Provider } from 'react-redux'
+import { Router } from 'react-router-dom'
+import history from './history'
+import store from './store'
+
 import Routes from './Routes'
 
 const App = () => {
   return (
-    <Routes/>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes/>
+        </Router>
+    </Provider>
   )
 }
 
